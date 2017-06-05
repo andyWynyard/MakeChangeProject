6a6e084013300c4d507fcdf151582c78dcb540f1
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-06-04 19:46:59 -0600

    Final commit i think

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Thus I created a copy to work on (for fear of breaking the first). I managed to 
 
 My final hand in is MakeChangeEfficient, however I will leave the original so you can see what I did firstly. Lets see if I can leave this alone ;)
 
-P.S. After all of the talk about the % operator, I wanted to see what I could do without it. I used >= instead to see how far that would take me. It works just fine, and I could have continued using this in the final class, but I decided to do it a % way.
+After all of the talk about the % operator, I wanted to see what I could do without it. I used >= instead to see how far that would take me. It works just fine, and I could have continued using this in the final class, but I decided to do it a % way.
+
+Late Sunday I changed the prompts to an array of prompts that could be added to if there were more prompts needed. This was cleaner than with strict Strings being passed to the user. 
+
+I also added another array to take the two amounts (price and money given) as I could then reduce the system input to one loop.
+
--- src/week1project/MakeChangeEfficient.java
+++ src/week1project/MakeChangeEfficient.java
@@ -3,57 +3,67 @@ import java.util.Scanner;
 
 public class MakeChangeEfficient {
 	static int[] changeDenoms = new int[12];
-
+	static String[] promptUser = {"Please enter item price: ", "Please enter the money tendered: ",
+					"Invalid amount!!", "Insufficient coin!!", "Required: ", "Your change includes: "};
+	static double inputArray[] = new double[2];
+	
 	public static void main(String[] args) {
+
 		TakeInput();
 	}
+	
+	/*********************************************************
+	 * 	I would very much like to compress this more
+	 * 	I think this whole method could be put into one array
+	 * 	I just have to figure out how to do it.
+	 *********************************************************/
 
 	public static void TakeInput() {
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Please enter item price: ");
-		double ticketPrice = keyboard.nextDouble();
-		System.out.print("Please enter the money tendered: ");
-		double moneyGiven = keyboard.nextDouble();
-		keyboard.close();
-		
-		if ((ticketPrice * 100 % 1) != 0 || (moneyGiven * 100 % 1) != 0) {
-			System.out.println("Invalid amount!!");
+		for (int i = 0; i < 2; i++) {
+			System.out.print(promptUser[i]);
+			inputArray[i] = keyboard.nextDouble(); // Ticket price and money given taken into array.
+		}
+		keyboard.close();	
+
+		if ((inputArray[0] * 100 % 1) != 0 || (inputArray[1] * 100 % 1) != 0) {
+			System.out.println(promptUser[2]);
 		} else {
-			if (ticketPrice > moneyGiven) {
-				System.out.println("Insufficient coin!!");
-				CalcChange(-ticketPrice, 0);			
-				System.out.println("Required: " + Concat());
+			if (inputArray[0] > inputArray[1]) {	// If this is true, more money is required, and the prompts inform as much.
+				System.out.println(promptUser[3]);
+				CalcChange(-inputArray[0], 0);	// Negative ticketPrice to invert as CalcChange takes 0 to compare.
+				System.out.println(promptUser[4] + Concat()); // Still uses the CalcChange and Concat methods to inform the user.
 			} else {
-				CalcChange(ticketPrice, moneyGiven);			
-				System.out.println("Your change includes: " + Concat());
+				CalcChange(inputArray[0], inputArray[1]);	// If all is well, this will happen.
+				System.out.println(promptUser[5] + Concat());
 			}
 		}
 	}
 	
 	public static void CalcChange(double ticketPrice, double moneyGiven) {
-		double returnSum = (moneyGiven * 100) - (ticketPrice * 100);
-		int[] noteGroup = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
+		double returnSum = (moneyGiven * 100) - (ticketPrice * 100); // (moneyGiven - ticketPrice) * 100, did not work for various denoms WHY!!!
+		int[] noteGroup = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1}; // * 100 removes decimal places, easier to work with.
 		for (int i = 0; i < changeDenoms.length; i++) {
-			if ((int)(returnSum / noteGroup[i]) % 1 == 0) {
-				changeDenoms[i] = (int)(returnSum / noteGroup[i]);	
+			if ((int)(returnSum / noteGroup[i]) % 1 == 0) { 	// Could be done with >=, really depends on the developer.
+				changeDenoms[i] = (int)(returnSum / noteGroup[i]);	// Casing removes the decimal to add to the changeDenoms array
 			} else {
-				changeDenoms[i] = 0;
+				changeDenoms[i] = 0; // Assigns value to not used part of array, could leave as null, but it was easier for debug.
 			}
-			returnSum = returnSum - (changeDenoms[i] * noteGroup[i]); 
+			returnSum = returnSum - (changeDenoms[i] * noteGroup[i]); // Keep changing the return sum after the multiples have been removed.
 		}
 	}
 	
 	public static String Concat() {
-		final String[] notesCoins = {" hundred(s), ", " fiftie(s), ", " twentie(s), ", 
-				" ten(s), ", " five(s), ", " two(s), ", " one(s), ", " fifty c(s), ", 
+		final String[] notesCoins = {" hundred(s), ", " fiftie(s), ", " twentie(s), ", // This could be made more efficient
+				" ten(s), ", " five(s), ", " two(s), ", " one(s), ", " fifty c(s), ",  // I don't know how as yet.
 				" quarter(s), ", " dime(s), ", " nickle(s(, ", " penny(s)."};
 		String concat = "";
 		
 		for (int i = 0; i < changeDenoms.length; i++) {
-			if (changeDenoms[i] != 0) {
-				concat += changeDenoms[i] + notesCoins[i];
+			if (changeDenoms[i] != 0) { 	//Make sure that at least one of the values exists
+				concat += changeDenoms[i] + notesCoins[i]; 	// 	Puts together the response for the user.
 			}
 		}
-		return concat;
+		return concat; 
 	}
 }
